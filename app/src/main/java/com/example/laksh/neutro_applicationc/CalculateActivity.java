85fc5503feb214d366e7e9ff711a0c79dca8906b
laksh <lakshan.1998@gmail.com>
2017-05-24 09:01:38 +0530

    Initial commit [REGISTER | LOGIN | PROFILE]

---
+++ app/src/main/java/com/example/laksh/neutro_applicationc/CalculateActivity.java
@@ -0,0 +1,14 @@
+package com.example.laksh.neutro_applicationc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CalculateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+
+    }
+}
---
+++ app/src/main/java/com/example/laksh/neutro_applicationc/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.laksh.neutro_applicationc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+    Button btnRegisterS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        btnRegisterS= (Button)findViewById(R.id.btnRegister);
+        btnRegisterS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+            }
+        });
+    }
+}
